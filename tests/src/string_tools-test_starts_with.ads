009e781b2e5730308f98eff8e03870707eb5e193
Matheus Xavier <soft.song@fastmail.net>
2025-06-04 01:55:22 +0200

    feat(tests): a few more tests on the string utils

--- tests/src/string_tools-test_starts_with.ads
+++ tests/src/string_tools-test_starts_with.ads
@@ -5,8 +5,8 @@ package String_Tools.Test_Starts_With is
 
    type Test is new AUnit.Simple_Test_Cases.Test_Case with null record;
 
-   function Name (T : Test) return AUnit.Message_String;
+   overriding function Name (T : Test) return AUnit.Message_String;
 
-   procedure Run_Test (T : in out Test);
+   overriding procedure Run_Test (T : in out Test);
 
 end String_Tools.Test_Starts_With;
